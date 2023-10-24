1bf803d3f681ec4813343379011d509e2bd41b0e
Sandrine Giron <sandrine.giron@outlook.com>
2023-10-24 09:45:24 +0200

    ajout page single-photo

---
+++ single-photo.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WordPress
+ * @subpackage motaphoto
+ * @since motaphoto 1.0
+ */
+?>
+
+ <?php get_header(); ?>
+
+
+ <?php get_footer(); ?>
